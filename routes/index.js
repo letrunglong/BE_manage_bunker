ac65652b63e69990dc2980f5a769e2e0baac1098
long_dev <letrunglong07@gmail.com>
2021-04-04 21:21:04 +0700

    4/4

--- routes/index.js
+++ routes/index.js
@@ -44,7 +44,7 @@ router.post('/register', (req, res, next) => {
 })
 //get products --WORKED
 router.get('/getProducts', (req, res, next) => {
-  const query = 'SELECT products.prod_id,	prod_name,cate_name,	prod_price,	prod_quantity,	prod_cate,	prod_bunker,	bunker_name,	prod_image,	unit_name,	prod_unit,	prod_producer,	producer_name,	prod_description FROM	products INNER JOIN producer    ON producer.producer_id = products.prod_producer INNER JOIN unit_prod u	ON u.unit_id = prod_unit INNER JOIN bunker b ON b.bunker_id = prod_bunker INNER JOIN categories cate ON cate.cate_id = prod_cate ORDER BY prod_name'
+  const query = 'SELECT products.prod_id,	prod_name, cate_name, price_sell, prod_price,	prod_quantity,	prod_cate,	prod_bunker,prod_image,	unit_name,	prod_unit,	prod_producer,	producer_name,	prod_description FROM	products INNER JOIN producer    ON producer.producer_id = products.prod_producer INNER JOIN unit_prod u	ON u.unit_id = prod_unit INNER JOIN categories cate ON cate.cate_id = prod_cate ORDER BY prod_name'
   pool.query(query, (error, response) => {
     if (response) {
       res.send(response.rows)
@@ -106,7 +106,7 @@ router.get('/get-producer', (req, res, next) => {
 })
 
 
-/***********************POST METHOD**********************************/ 
+/***********************POST METHOD**********************************/
 
 //upload image --WORKED
 const fileStorageEngine = multer.diskStorage({
@@ -120,12 +120,14 @@ const fileStorageEngine = multer.diskStorage({
 const upload = multer({ storage: fileStorageEngine })
 //add products --WORKED
 router.post('/add-products', upload.single("image"), (req, res) => {
-  let { product_name, product_bunker, product_cate, product_price, product_producer, product_quantity, product_unit, product_image } = req.body
-  pool.query("INSERT INTO products(prod_name,prod_price,prod_quantity,prod_cate,prod_bunker,prod_image,prod_unit,prod_producer) values ($1,$2,$3,$4,$5,$6,$7,$8)", [product_name, product_price, product_quantity, product_cate, product_bunker, product_image, product_unit, product_producer], (error, response) => {
+  console.log(req.body);
+  let { product_name, product_bunker, product_cate, product_price, product_producer, product_quantity, product_unit, product_image,product_description,product_sell } = req.body
+ let query = "INSERT INTO public.products( prod_name, prod_price, prod_quantity, prod_cate, prod_image, prod_unit, prod_producer, prod_description, price_sell)VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)"
+  pool.query(query, [product_name, product_price, product_quantity, product_cate, product_image, product_unit, product_producer,product_description,product_sell], (error, response) => {
     try {
       if (response) {
         return res.status(200).json({ status: 200, messages: "Thêm sản phẩm thành công " })
-      }else if(error){
+      } else if (error) {
         return res.status(200).json({ status: 404, messages: "Thêm sản phẩm thất bại " })
       }
     } catch {
@@ -135,49 +137,82 @@ router.post('/add-products', upload.single("image"), (req, res) => {
 })
 
 // POST ADD CATEGORIES --WORKED
-router.post('/add-categories',(req,res,next)=>{
-  let {cate_name,cate_description} = req.body
-  pool.query('INSERT INTO categories (cate_name,cate_description) VALUES ($1,$2)',[cate_name,cate_description],(error,response)=>{
-    try{
-    if(response){
-      return res.status(200).json({ status: 200, messages: "Thêm danh mục thành công" })
-    } else if(error){
+router.post('/add-categories', (req, res, next) => {
+  let { cate_name, cate_description } = req.body
+  pool.query('INSERT INTO categories (cate_name,cate_description) VALUES ($1,$2)', [cate_name, cate_description], (error, response) => {
+    try {
+      if (response) {
+        return res.status(200).json({ status: 200, messages: "Thêm danh mục thành công" })
+      } else if (error) {
+        return res.status(200).json({ status: 400, messages: "Thêm danh mục thất bại!" })
+      }
+    } catch {
       return res.status(200).json({ status: 400, messages: "Thêm danh mục thất bại!" })
     }
-  }catch{
-    return res.status(200).json({ status: 400, messages: "Thêm danh mục thất bại!" })
-  }
   })
 })
-// POST ADD PRODUCER --TESTING
-router.post('/add-producer',(req,res,next)=>{
-  let {producer_name,producer_description} = req.body
-  pool.query('INSERT INTO categories (producer_name,producer_description) VALUES ($1,$2)',[producer_name,producer_description],(error,response)=>{
-    try{
-    if(response){
-      return res.status(200).json({ status: 200, messages: "Thêm nhà sản xuất thành công" })
-    } else if(error){
-      return res.status(200).json({ status: 400, messages: "Thêm nhà sản xuất thất bại!" })
+// POST ADD PRODUCER --WORKED
+router.post('/add-producer', (req, res, next) => {
+  let { producer_name, producer_description } = req.body
+  console.log(req.body);
+  pool.query('INSERT INTO producer (producer_name,producer_description) VALUES ($1,$2)', [producer_name, producer_description], (error, response) => {
+    try {
+      if (response) {
+        return res.status(200).json({ status: 200, messages: "Thêm nhà sản xuất thành công" })
+      } else if (error) {
+        return res.status(200).json({ status: 400, messages: "Thêm nhà sản xuất thất bại!" })
+      }
+    } catch {
+      res.send('Failure!')
     }
-  }catch{
-    res.send('Failure!')
-  }
+  })
+})
+// POST ADD UNIT --TEST
+router.post('/add-unit', (req, res, next) => {
+  let { unit_name, unit_description } = req.body
+  console.log(req.body);
+  pool.query('INSERT INTO unit_prod (unit_name,unit_description) VALUES ($1,$2)', [unit_name, unit_description], (error, response) => {
+    try {
+      if (response) {
+        return res.status(200).json({ status: 200, messages: "Thêm đơn vị tính thành công" })
+      } else if (error) {
+        return res.status(200).json({ status: 400, messages: "Thêm đơn vị tính thất bại!" })
+      }
+    } catch {
+      res.send('Failure!')
+    }
   })
 })
 
 // POST ADD BUNKER --WORKED
-router.post('/add-bunker',(req,res,next)=>{
-  let {bunker_name,bunker_location} = req.body
-  pool.query('INSERT INTO bunker (bunker_name,bunker_location) VALUES ($1,$2)',[bunker_name,bunker_location],(error,response)=>{
-    try{
-    if(response){
-      return res.status(200).json({ status: 200, messages: "Thêm nhà kho thành công" })
-    } else if(error){
-      return res.status(200).json({ status: 400, messages: "Thêm nhà kho thất bại!" })
+router.post('/add-bunker', (req, res, next) => {
+  pool.query('INSERT INTO bunker (bunker_name,bunker_location) VALUES ($1,$2)', [bunker_name, bunker_location], (error, response) => {
+    try {
+      if (response) {
+        return res.status(200).json({ status: 200, messages: "Thêm nhà kho thành công" })
+      } else if (error) {
+        return res.status(200).json({ status: 400, messages: "Thêm nhà kho thất bại!" })
+      }
+    } catch {
+      return res.status(200).json({ status: 400, messages: "Lỗi" })
     }
-  }catch{
-    res.send('Failure!')
-  }
   })
 })
+/***********************DELETE METHOD**********************************/
+
+router.delete('/delete-product/:id', (req, res, next) => {
+  let { id } = req.params
+  pool.query('DELETE FROM products WHERE prod_id = ' + id, (err, response) => {
+    try {
+      if (response) {
+        return res.status(200).json({ status: 200, messages: "Xóa sản phẩm thành công" })
+      } else if (err) {
+        return res.status(200).json({ status: 200, messages: "Xóa sản phẩm thất bại" })
+      }
+    } catch {
+      return res.send("Failure!")
+    }
+  })
+})
+
 module.exports = router;
